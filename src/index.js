72a62bcf9bad20b9f16f5dcb09b75f3c23f2a6c3
Fishfaceeast <peytonyu@126.com>
2019-05-14 22:38:32 +0800

    code_splitting-dynamic import async/await

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import './style.css';
 
-function getComponent() {
-   return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+async function getComponent() {
+    const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
-
-  }).catch(error => 'An error occurred while loading the component');
 }
 getComponent().then(component => {
   document.body.appendChild(component);
