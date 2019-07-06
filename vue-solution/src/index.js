1b62d5f323f65370582c43db57e56c21bf54260d
Fishfaceeast <peytonyu@126.com>
2019-07-06 17:02:00 +0800

    set up vue-solution folder

---
+++ vue-solution/src/index.js
@@ -0,0 +1,16 @@
+// require('./style.css');
+import './style.css';
+import './index.scss';
+import { cube } from './math.js';
+
+function component() {
+  const element = document.createElement('pre');
+  element.innerHTML = [
+     'Hello webpack!',
+     '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ vue-solution/webpack.dev.js
@@ -0,0 +1,32 @@
+/**
+ * Created by yuqian on 2019/5/16.
+ */
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  }
+});
